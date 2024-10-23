425986139e5cb61a498f38529df8ffa32f1205d5
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2024-10-23 12:33:07 +0700

    Remove docs dir in url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -91,6 +91,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
+          routeBasePath: "",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,7 @@
 import { Redirect } from '@docusaurus/router';
-import HomeSvg from '@site/static/img/Blog.svg';
-
-function HomepageHeader() {
-  return (
-    <div className='w-full h-fit bg-[#3e3e42] p-10 flex flex-row justify-between gap-20'>
-      <div className='w-[70%]'>
-        <div className='text-4xl text-neutral-50'>zkDatabase Documentation</div>
-        <div className='text-lg text-neutral-50 pt-8'>
-          Welcome to the official zkDatabase Documentation. Whether you're a developer, database administrator, or just starting your journey with zkDatabase, our documentation provides you with the information and knowledge needed to build applications on zkDatabase.
-        </div>
-      </div>
-      <div className='w-[30%] flex items-center justify-center'>
-        <HomeSvg />
-      </div>
-    </div>
-  );
-}
 
 export default function Home(): JSX.Element {
   return (
-    <Redirect to="/docs/intro" />
+    <Redirect to="/intro" />
   );
 }
